fe4b00c4016c96bb2f95afe6578c56c0061d1210
N-Wilson11 <91117438+N-Wilson11@users.noreply.github.com>
2025-04-27 02:08:02 +0200

    fix lang dropdown bug

--- my-laravel-project/app/Http/Controllers/LanguageController.php
+++ my-laravel-project/app/Http/Controllers/LanguageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Log;
 
 class LanguageController extends Controller
 {
@@ -16,13 +17,25 @@ class LanguageController extends Controller
      */
     public function changeLanguage($locale)
     {
+        // Log voor debugging
+        Log::info('Taalwissel poging', ['locale' => $locale]);
+        
         // Only allow 'en' or 'nl' locales
         if ($locale === 'en' || $locale === 'nl') {
             // Store the locale in the session
             Session::put('locale', $locale);
             
+            // Force the session to be saved immediately
+            Session::save();
+            
             // Set the locale for the current request
             App::setLocale($locale);
+            
+            Log::info('Taal succesvol gewisseld', [
+                'locale' => $locale, 
+                'session_id' => Session::getId(),
+                'has_locale_in_session' => Session::has('locale')
+            ]);
         }
         
         return redirect()->back();
